19ab161242f8627d75e94a676c99499cceacc6c6
manshidkm <manshidkm@gmail.com>
2024-11-04 09:33:26 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,22 @@
+
+//     var a=5;
+//     a=8;
+//     var b=5;
+//     var sum=a+b;
+    
+
+
+// console.log(sum);
+
+
+let a=8;
+let b=6;
+
+if(a<=b){
+    console.log("lghvg");
+    
+}
+else{
+    console.log("not g");
+    
+}
---
+++ javaa.js
@@ -0,0 +1,28 @@
+// var a=2;
+// var b=5;
+// var sum=a+b;
+// console.log(sum);
+
+
+// var a=5;
+// var square=(a*a)
+// console.log(square);
+
+// var a=4;
+// var b=8;
+// var area=(a*b)
+// console.log(area);
+// var a=7;
+// var b=5;
+// var perimeter=2*(a+b);
+// console.log(perimeter);
+
+var a=6;
+var b=8;
+var c=7;
+var avarage=(a+b+c)/3;
+console.log(avarage);
+
+
+
+
